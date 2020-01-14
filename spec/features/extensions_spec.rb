c379775a9b957e6a0522396412e5842ec1d53e52
Rustam G <vasil.brazil@gmail.com>
2020-01-14 13:52:20 +0300

    add spec for median

--- spec/features/extensions_spec.rb
+++ spec/features/extensions_spec.rb
@@ -4,7 +4,7 @@ describe 'Extensions' do
   describe 'string_agg' do
     before { DB.extension :string_agg }
 
-    it 'generates correct sql for Redshift' do      
+    it 'generates correct sql for Redshift' do
       allow(DB).to receive(:adapter_scheme).and_return(:redshift)
       expect(
         DB[:test].
@@ -29,6 +29,34 @@ describe 'Extensions' do
               as(:relative)
           ).sql).to eq(
             "SELECT string_agg(CAST(\"revenue\" AS varchar(255)), ',' ORDER BY \"period_start\" ASC, \"sub_period_start\" ASC) AS \"relative\" FROM \"test\""
+          )
+    end
+  end
+
+  describe 'median' do
+    before { DB.extension :median }
+
+    it 'generates correct sql for Redshift' do
+      allow(DB).to receive(:adapter_scheme).and_return(:redshift)
+      expect(
+        DB[:test].
+          select(
+            Sequel.
+              median(:revenue)
+          ).sql).to eq(
+            "SELECT median(\"revenue\") FROM \"test\""
+          )
+    end
+
+    it 'generates correct sql for Postgresql' do
+      allow(DB).to receive(:adapter_scheme).and_return(:postgres)
+      expect(
+        DB[:test].
+          select(
+            Sequel.
+              median(:revenue)
+          ).sql).to eq(
+            "SELECT percentile_disc(0.5) WITHIN GROUP (ORDER BY \"revenue\") FROM \"test\""
           )
     end
   end
